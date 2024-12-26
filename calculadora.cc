8a2d53215f982699677ef4d3982ad7c1451a2be2
pgrd1 <pgrd1@alu.ua.es>
2024-12-26 23:06:37 +0100

    Outputs actualizados

--- calculadora.cc
+++ calculadora.cc
@@ -13,17 +13,17 @@ int main() {
 
     switch (op) {
         case '+':
-            cout << "Resultado de la suma: " << a + b << endl;
+            cout << "Resultado de la SUMA: " << a + b << endl;
             break;
         case '-':
-            cout << "Resultado de la resta: " << a - b << endl;
+            cout << "Resultado de la RESTA: " << a - b << endl;
             break;
         case '*':
-            cout << "Resultado de la multiplicación: " << a * b << endl;
+            cout << "Resultado de la MULTIPLICACIÓN: " << a * b << endl;
             break;
         case '/':
             if (b != 0)
-                cout << "Resultado: " << a / b << endl;
+                cout << "Resultado de la DIVISIÓN: " << a / b << endl;
             else
                 cout << "Error: División por cero." << endl;
             break;
